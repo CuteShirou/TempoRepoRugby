d1b70765a8bafa3910cabf0f252208246a493471
Valentin <111918150+CuteShirou@users.noreply.github.com>
2024-11-15 13:26:55 +0100

    [NEW]

---
+++ Rugby Games/LightEngine/Player.cpp
@@ -0,0 +1,5 @@
+#include "Player.h"
+
+Player::Player(float radius, const sf::Color& color) : Entity(radius, color)
+{
+}
---
+++ Rugby Games/LightEngine/Player.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Entity.h"
+
+class Player : public Entity
+{
+public:
+	Player(float radius, const sf::Color& color);
+
+
+private:
+
+};
+
